4cc9c8470232dc2a9613ffded7817db4e00617f8
Vivek Thakur <vivek78643@gmail.com>
2021-07-24 23:10:57 +0530

    post images

--- src/post/utils/postModel.ts
+++ src/post/utils/postModel.ts
@@ -2,6 +2,10 @@ import * as mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema({
   body: String,
+  images: {
+    type: [String],
+    default: [],
+  },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
