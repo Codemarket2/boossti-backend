fcb7ac85a2e34fa3158b59f907756eb0a61d8350
Vivek Thakur <vivek78643@gmail.com>
2021-09-17 15:33:21 +0530

    data migration script

--- src/utils/DB.ts
+++ src/utils/DB.ts
@@ -7,15 +7,18 @@ export const DB = async (DB_STRING?: string) => {
     if (isConnected) {
       console.log('=> using existing database connection');
       return;
-    } else if (!process.env.DATABASE) {
+    } else if (!process.env.DATABASE && !DB_STRING) {
       throw new Error('Database connection string not found');
     } else {
-      const db = await mongoose.connect(DB_STRING || process.env.DATABASE, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-      });
+      const db = await mongoose.connect(
+        DB_STRING || process.env.DATABASE || '',
+        {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true,
+        }
+      );
       isConnected = db.connections[0].readyState;
       console.log('DB Connection Successfull!');
       return;
