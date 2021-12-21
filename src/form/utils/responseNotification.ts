fac62a09a222af59eca4c83a197885b6e97f684d
Vivek Thakur <vivek78643@gmail.com>
2021-12-21 18:01:10 +0530

    bug fix

--- src/form/utils/responseNotification.ts
+++ src/form/utils/responseNotification.ts
@@ -1,11 +1,15 @@
 import { sendNotification } from '../../notification/utils/sendNotification';
 
-export const sendResponseNotification = async (ownerId: any, responseId: string) => {
+export const sendResponseNotification = async (ownerId: any, response: any) => {
+  let submitedBy = '';
+  if (response?.createdBy?.name) {
+    submitedBy = `by ${response?.createdBy?.name}`;
+  }
   const payload = {
     userId: ownerId,
     title: 'Form Submission',
-    description: 'You have new form submission',
-    link: `/response/${responseId}`,
+    description: `You have new form submission ${submitedBy}`,
+    link: `/response/${response?._id}`,
   };
   await sendNotification(payload);
 };
