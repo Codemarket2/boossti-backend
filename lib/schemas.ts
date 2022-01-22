acaeee13c5f323f4e5b84f8b69bd904a503acd60
Vivek Thakur <vivek78643@gmail.com>
2022-01-23 00:55:30 +0530

    comment guest query

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -4,4 +4,6 @@ export default [
   'src/notification/notification.graphql',
   'src/list/list.graphql',
   'src/field/field.graphql',
+  'src/like/like.graphql',
+  'src/comment/comment.graphql',
 ];
