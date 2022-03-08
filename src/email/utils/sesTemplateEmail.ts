c6099023d916c27d29c2ab1f0a391c17609ea977
sonukumar81800 <sonu.patna0808@gmail.com>
2022-03-08 16:06:47 +0530

    Adding Send Bulk Email one by one

--- src/email/utils/sesTemplateEmail.ts
+++ src/email/utils/sesTemplateEmail.ts
@@ -1,32 +1,32 @@
 import * as AWS from 'aws-sdk';
 import { createTemplate, deleteTemplate } from './sesCreateEmailTemplate';
 export const sendBulkTemplatedEmail = (data) => {
-  const params = {
-    Destinations: data.collection_of_email_scraped.map((c) => ({
-      Destination: {
-        ToAddresses: c.emails,
-      },
-      ReplacementTags: data.defaultTags,
-      ReplacementTemplateData: JSON.stringify(
-        {
-          business_name: c.business_name,
-          website_link: c.website_link,
-          telephone: c.telephone,
-          postal_code: c.postal_code,
-          state: c.state,
-          city: c.city,
-          keyword: c.keyword[0],
-          Address_line1: c.Address_line1,
-        } || {},
-      ),
-    })),
-    ConfigurationSetName: 'vivek-mongo',
-    Source: 'info@boossti.com',
-    Template: data.templateName,
-    DefaultTags: data.defaultTags,
-    DefaultTemplateData: JSON.stringify(data.defaultTemplateData || {}),
-  };
-  return new AWS.SES({ region: 'us-east-1' }).sendBulkTemplatedEmail(params).promise();
+  //   const params = {
+  //     Destinations: data.collection_of_email_scraped.map((c) => ({
+  //       Destination: {
+  //         ToAddresses: c.emails,
+  //       },
+  //       ReplacementTags: data.defaultTags,
+  //       ReplacementTemplateData: JSON.stringify(
+  //         {
+  //           business_name: c.business_name,
+  //           website_link: c.website_link,
+  //           telephone: c.telephone,
+  //           postal_code: c.postal_code,
+  //           state: c.state,
+  //           city: c.city,
+  //           keyword: c.keyword[0],
+  //           Address_line1: c.Address_line1,
+  //         } || {},
+  //       ),
+  //     })),
+  //     ConfigurationSetName: 'vivek-mongo',
+  //     Source: 'info@boossti.com',
+  //     Template: data.templateName,
+  //     DefaultTags: data.defaultTags,
+  //     DefaultTemplateData: JSON.stringify(data.defaultTemplateData || {}),
+  //   };
+  //   return new AWS.SES({ region: 'us-east-1' }).sendBulkTemplatedEmail(params).promise();
 };
 
 export const sendBulkEmails = async (data) => {
