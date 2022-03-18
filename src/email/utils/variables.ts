47f36b9a9d117f160cc2e796d154a99ea67c68d5
boossti <boossti01@gmail.com>
2022-03-18 15:25:08 -0700

    now contact schema values can be included on subject and message while sending emails from UI

--- src/email/utils/variables.ts
+++ src/email/utils/variables.ts
@@ -1,7 +1,8 @@
-export const replaceVariables = (text, values) => {
-  let newText;
-  for (let key in values) {
-    newText = text.split(`{{${key}}}`).join(values[key] || '');
+export const replaceVariables = (text, contact) => {
+  let newText = text;
+  const values = { ...contact.toObject() };
+  for (const key in values) {
+    newText = newText.split(`{{${key}}}`).join(values[key] || '');
   }
-  return newText || text;
+  return newText;
 };
