3880b2b335b575c29db580906ead409a8540a3b0
Vivek Thakur <vivek78643@gmail.com>
2022-06-22 06:45:41 +0530

    bug fix

--- data-migration/system-forms.ts
+++ data-migration/system-forms.ts
@@ -1,4 +1,5 @@
 import { FormModel } from '../src/form/utils/formModel';
+import { ResponseModel } from '../src/form/utils/responseModel';
 import { db } from './db';
 
 const userForm = {
@@ -16,7 +17,30 @@ const createSystemForm = async () => {
   console.log(form);
 };
 
+const createUser = async ({ firstName, lastName, email }) => {
+  const form = await FormModel.findOne({ slug: 'users' });
+  if (form?._id) {
+    const userResponse: any = {
+      formId: form?._id,
+      values: [],
+      count: 2,
+    };
+    form?.fields?.forEach((field) => {
+      if (field?.label?.toLowerCase() === 'first name') {
+        userResponse.values.push({ value: firstName, field: field?._id });
+      } else if (field?.label?.toLowerCase() === 'last name') {
+        userResponse.values.push({ value: lastName, field: field?._id });
+      } else if (field?.label?.toLowerCase() === 'email') {
+        userResponse.values.push({ value: email, field: field?._id });
+      }
+    });
+    const response = await ResponseModel.create(userResponse);
+    console.log({ response });
+  }
+};
+
 (async () => {
   await db();
-  await createSystemForm();
+  // await createUser({ firstName: '', lastName: '', email: '' });
+  // await createSystemForm();
 })();
