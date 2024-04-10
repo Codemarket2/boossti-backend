48c81b2f9c84477f3ffe5e93e9bc33da8ebada7f
naresh parmar <ncparmar8888@gmail.com>
2024-04-10 17:22:53 +0530

    upgrade the version

--- lib/MyStack.ts
+++ lib/MyStack.ts
@@ -1,8 +1,8 @@
 import * as sst from '@serverless-stack/resources';
-import { StringParameter } from '@aws-cdk/aws-ssm';
-import { AuthorizationType, UserPoolDefaultAction } from '@aws-cdk/aws-appsync';
-import { UserPool } from '@aws-cdk/aws-cognito';
-import { Expiration, Duration } from '@aws-cdk/core';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { AuthorizationType, UserPoolDefaultAction } from '@aws-cdk/aws-appsync-alpha';
+import { UserPool } from 'aws-cdk-lib/aws-cognito';
+import { Expiration, Duration } from 'aws-cdk-lib';
 import dataSources from './dataSources';
 import resolvers from './resolvers';
 import schemas from './schemas';
@@ -41,42 +41,46 @@ export default class MyStack extends sst.Stack {
     const userPol = UserPool.fromUserPoolId(this, 'UserPool', USER_POOL_ID);
 
     // Create the AppSync GraphQL API
-    const api = new sst.AppSyncApi(this, 'graphql', {
-      graphqlApi: {
-        schema: schemas,
-        authorizationConfig: {
-          defaultAuthorization: {
-            authorizationType: AuthorizationType.USER_POOL,
-            userPoolConfig: {
-              userPool: userPol,
-              defaultAction: UserPoolDefaultAction.ALLOW,
-            },
-          },
-          additionalAuthorizationModes: [
-            {
-              authorizationType: AuthorizationType.API_KEY,
-              apiKeyConfig: {
-                expires: Expiration.after(Duration.days(365)),
+    const api = new sst.AppSyncApi(this, 'GraphqlApi', {
+      schema: schemas,
+      cdk: {
+        graphqlApi: {
+          authorizationConfig: {
+            defaultAuthorization: {
+              authorizationType: AuthorizationType.USER_POOL,
+              userPoolConfig: {
+                userPool: userPol,
+                defaultAction: UserPoolDefaultAction.ALLOW,
               },
             },
-          ],
+            additionalAuthorizationModes: [
+              {
+                authorizationType: AuthorizationType.API_KEY,
+                apiKeyConfig: {
+                  expires: Expiration.after(Duration.days(365)),
+                },
+              },
+            ],
+          },
         },
       },
-      defaultFunctionProps: {
-        timeout: 60,
-        environment: {
-          SENDER_EMAIL: SENDER_EMAIL || '',
-          DATABASE: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@codemarket-staging.k16z7.mongodb.net/${scope.stage}?retryWrites=true&w=majority`,
-          USER_POOL_ID: USER_POOL_ID,
-          SNS_ORIGINAL_NUMBER: SNS_ORIGINAL_NUMBER,
-          GRAPHQL_API_URL: process.env.GRAPHQL_API_URL || '',
-          GRAPHQL_API_KEY: process.env.GRAPHQL_API_KEY || '',
-          ONESIGNAL_API_KEY: process.env.ONESIGNAL_API_KEY || '',
-          ONESIGNAL_APP_ID: process.env.ONESIGNAL_APP_ID || '',
-          DISTRIBUTION_ID: DISTRIBUTION_ID || '',
-          FRONTEND_URL,
-          STAGE: scope.stage,
-          USERS_FORM_SLUG,
+      defaults: {
+        function: {
+          timeout: 60,
+          environment: {
+            SENDER_EMAIL: SENDER_EMAIL || '',
+            DATABASE: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@codemarket-staging.k16z7.mongodb.net/${scope.stage}?retryWrites=true&w=majority`,
+            USER_POOL_ID: USER_POOL_ID,
+            SNS_ORIGINAL_NUMBER: SNS_ORIGINAL_NUMBER,
+            GRAPHQL_API_URL: process.env.GRAPHQL_API_URL || '',
+            GRAPHQL_API_KEY: process.env.GRAPHQL_API_KEY || '',
+            ONESIGNAL_API_KEY: process.env.ONESIGNAL_API_KEY || '',
+            ONESIGNAL_APP_ID: process.env.ONESIGNAL_APP_ID || '',
+            DISTRIBUTION_ID: DISTRIBUTION_ID || '',
+            FRONTEND_URL,
+            STAGE: scope.stage,
+            USERS_FORM_SLUG,
+          },
         },
       },
       dataSources: dataSources,
@@ -95,8 +99,8 @@ export default class MyStack extends sst.Stack {
     });
 
     // // Enable the AppSync API to access the DynamoDB table
-    api.attachPermissions(sst.PermissionType.ALL);
-    csvFunction.attachPermissions(sst.PermissionType.ALL);
+    api.attachPermissions('*');
+    csvFunction.attachPermissions('*');
 
     // const graphqlApiUrl = new StringParameter(this, scope.stage, {
     //   parameterName: `/boossti/graphql-api-url/${scope.stage}`,
@@ -116,10 +120,10 @@ export default class MyStack extends sst.Stack {
 
     // Show the AppSync API Id in the output
     this.addOutputs({
-      ApiId: api.graphqlApi.apiId,
-      GraphqlUrl: api.graphqlApi.graphqlUrl,
+      ApiId: api.cdk.graphqlApi.apiId,
+      GraphqlUrl: api.cdk.graphqlApi.graphqlUrl,
       // @ts-expect-error because some api will not have apiKey
-      ApiKey: api.graphqlApi.apiKey,
+      ApiKey: api.cdk.graphqlApi.apiKey,
       FunctionName: csvFunction.functionName,
       // graphqlURL: graphqlURL.stringValue,
     });
